1f5ab72b248cbfb059ffbfc3bfa160bf0066c008
Şafak Şahin <safaksahin022@gmail.com>
2024-02-04 14:09:56 +0300

    Meyawo Portfolio
    Dynamic CV website with admin panel

---
+++ MeyawoPortfolio/Controllers/ContactController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MeyawoPortfolio.Models;
+
+namespace MeyawoPortfolio.Controllers
+{
+    public class ContactController : Controller
+    {
+        DbMyPortfolioEntities db = new DbMyPortfolioEntities();
+        public ActionResult Index()
+        {
+            var values = db.TblContact.ToList();
+            return View(values);
+        }
+
+        public ActionResult DeleteContact(int id)
+        {
+            var value = db.TblContact.Find(id);
+            db.TblContact.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult ContactDetail(int id)
+        {
+            var message = db.TblContact.FirstOrDefault(x => x.ContactID == id);
+
+            if (message == null)
+            {
+                // Mesaj bulunamazsa, hata sayfasına yönlendirme yapabilirsiniz
+                return RedirectToAction("Error");
+            }
+
+            return View(message);
+        }
+
+        public ActionResult MarkAsRead(int id) // Okundu bilgisini güncelleme
+        {
+            var message = db.TblContact.Find(id);
+
+            if (message != null)
+            {
+                message.IsRead = true;
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("ContactDetail", new { id = id });
+        }
+    }
+}
---
+++ MeyawoPortfolio/Controllers/StatisticController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MeyawoPortfolio.Models;
+
+namespace MeyawoPortfolio.Controllers
+{
+    public class StatisticController : Controller
+    {
+        DbMyPortfolioEntities db = new DbMyPortfolioEntities();
+        public ActionResult Index()
+        {
+            ViewBag.categoryCount = db.TblCategory.Count();
+            ViewBag.projectCount = db.TblProject.Count();
+            ViewBag.messageCount = db.TblContact.Count();
+            ViewBag.csProjectCount = db.TblProject.Where(x => x.Projectcategory == 3).Count();
+            ViewBag.isNotReadMessageCount = db.TblContact.Where(x => x.IsRead == false).Count();
+            ViewBag.lastProjectName = db.LastProjectName().FirstOrDefault();
+            ViewBag.testimonialCount = db.TblTestimonial.Count();
+            return View();
+        }
+    }
+}
